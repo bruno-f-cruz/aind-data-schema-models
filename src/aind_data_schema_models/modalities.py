00c9d4346bfe49d2bdcd39c123c840d1f8c8fd3c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-20 14:26:38 -0700

    Implement ophys to pophys coercion

--- src/aind_data_schema_models/modalities.py
+++ src/aind_data_schema_models/modalities.py
@@ -1,10 +1,21 @@
 """Module for Modality definitions"""
 
+from typing import Any
+
 from importlib_resources import files
-from pydantic import ConfigDict, Field
+from pydantic import BeforeValidator, ConfigDict, Field
 
 from aind_data_schema_models.pid_names import BaseName
 from aind_data_schema_models.utils import create_literal_class, read_csv
+
+
+# This is a hotfix to allow users to use the old ophys modality abbreviation
+# It should be removed in a future release
+def _coerce_ophys_to_pophys(v: Any):
+    if isinstance(v, dict):
+        if v.get("abbreviation") == "ophys":
+            return Modality.POPHYS
+    return v
 
 
 class ModalityModel(BaseName):
@@ -21,6 +32,7 @@ Modality = create_literal_class(
     base_model=ModalityModel,
     discriminator="abbreviation",
     class_module=__name__,
+    validators=[BeforeValidator(_coerce_ophys_to_pophys)],
 )
 
 Modality.abbreviation_map = {m().abbreviation: m() for m in Modality.ALL}
