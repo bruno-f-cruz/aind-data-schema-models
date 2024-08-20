661046d628f5aef47293bfdece3b9dcdaa287c4f
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-20 14:26:47 -0700

    Add tests for coercion

--- tests/test_modalities.py
+++ tests/test_modalities.py
@@ -2,7 +2,15 @@
 
 import unittest
 
+from pydantic import BaseModel
+
 from aind_data_schema_models.modalities import Modality
+
+
+class MockModel(BaseModel):
+    mod1: Modality.ONE_OF
+    mod2: Modality.ONE_OF
+    mod3: Modality.ONE_OF
 
 
 class TestModality(unittest.TestCase):
@@ -12,6 +20,19 @@ class TestModality(unittest.TestCase):
         """Tests modality can be constructed from abbreviation"""
 
         self.assertEqual(Modality.ECEPHYS, Modality.from_abbreviation("ecephys"))
+
+    def test_ophys_to_pophys_coercion(self):
+        """Tests that ophys is coerced to pophys"""
+
+        _test_literal = """
+        {
+        "mod1":{"name":"Extracellular electrophysiology","abbreviation":"ecephys"},
+        "mod2":{"name":"Planar optical physiology","abbreviation":"pophys"},
+        "mod3":{"name":"foo bar","abbreviation":"ophys"}
+        }"""
+        t = MockModel(mod1=Modality.ECEPHYS, mod2=Modality.POPHYS, mod3=Modality.POPHYS)
+
+        self.assertEqual(t, MockModel.model_validate_json(_test_literal))
 
 
 if __name__ == "__main__":
