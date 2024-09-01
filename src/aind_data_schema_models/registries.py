eda471b85b8f27a98dce55405bee17e801afc3c1
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-31 22:18:18 -0700

    Automatically generated registries

--- src/aind_data_schema_models/registries.py
+++ src/aind_data_schema_models/registries.py
@@ -2,33 +2,10 @@
 
 from typing import Union
 
-from importlib_resources import files
-from pydantic import ConfigDict, Field
+from pydantic import Field
 from typing_extensions import Annotated
 
-from aind_data_schema_models.pid_names import BaseName
-from aind_data_schema_models.utils import create_literal_class, read_csv
-
-
-class RegistryModel(BaseName):
-    """Base model config"""
-
-    model_config = ConfigDict(frozen=True)
-
-    name: str = Field(..., title="Registry name")
-    abbreviation: str = Field(..., title="Registry abbreviation")
-
-
-Registry = create_literal_class(
-    objects=read_csv(str(files("aind_data_schema_models.models").joinpath("registries.csv"))),
-    class_name="Registry",
-    base_model=RegistryModel,
-    discriminator="abbreviation",
-    class_module=__name__,
-)
-
-Registry.abbreviation_map = {m().abbreviation: m() for m in Registry.ALL}
-Registry.from_abbreviation = lambda x: Registry.abbreviation_map.get(x)
+from aind_data_schema_models._generated.registries import _Registry as Registry
 
 
 def map_registry(abbreviation: str, record: dict):
