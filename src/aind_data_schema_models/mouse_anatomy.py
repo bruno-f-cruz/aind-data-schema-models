e180652dfb980d4ab029042d67a64b130cc25082
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-10-10 12:47:29 -0700

    Refactor top level modules to import autogen classes

--- src/aind_data_schema_models/mouse_anatomy.py
+++ src/aind_data_schema_models/mouse_anatomy.py
@@ -1,45 +1,14 @@
 """Module for Mouse Anatomy"""
 
-from typing_extensions import Annotated
-from importlib_resources import files
-from pydantic import BaseModel, ConfigDict, Field
-
-from aind_data_schema_models.registries import Registry, RegistryModel
-from aind_data_schema_models.utils import create_literal_class, read_csv, one_of_instance
-
-
-class MouseAnatomyModel(BaseModel):
-    """Base model config"""
-
-    model_config = ConfigDict(frozen=True)
-
-    name: str = Field(..., title="Structure name")
-    registry: Annotated[RegistryModel, Field(default=Registry.from_abbreviation("EMAPA"))]
-    registry_identifier: str = Field(..., title="Structure EMAPA ID")
-
-
-mouse_objects = read_csv(str(files("aind_data_schema_models.models").joinpath("mouse_dev_anat_ontology.csv")))
-
-MouseAnatomicalStructure = create_literal_class(
-    objects=mouse_objects,
-    class_name="MouseAnatomyType",
-    base_model=MouseAnatomyModel,
-    discriminator="registry_identifier",
-    class_module=__name__,
-)
+from aind_data_schema_models._generated.mouse_anatomy import _MouseAnatomyType as MouseAnatomicalStructure
+from aind_data_schema_models.utils import one_of_instance
 
 MouseAnatomicalStructure.EMG_MUSCLES = one_of_instance(
     [
         MouseAnatomicalStructure.DELTOID,
         MouseAnatomicalStructure.PECTORALIS_MAJOR,
         MouseAnatomicalStructure.TRICEPS_BRACHII,
-        MouseAnatomicalStructure.LATERAL_HEAD_OF_TRICEPS_BRACHII,
-        MouseAnatomicalStructure.LONG_HEAD_OF_TRICEPS_BRACHII,
-        MouseAnatomicalStructure.MEDIAL_HEAD_OF_TRICEPS_BRACHII,
         MouseAnatomicalStructure.BICEPS_BRACHII,
-        MouseAnatomicalStructure.LONG_HEAD_OF_BICEPS_BRACHII,
-        MouseAnatomicalStructure.SHORT_HEAD_OF_BICEPS_BRACHII,
-        MouseAnatomicalStructure.TENDON_OF_BICEPS_BRACHII,
         MouseAnatomicalStructure.PARS_SCAPULARIS_OF_DELTOID,
         MouseAnatomicalStructure.EXTENSOR_CARPI_RADIALIS_LONGUS,
         MouseAnatomicalStructure.EXTENSOR_DIGITORUM_COMMUNIS,
--- src/aind_data_schema_models/registries.py
+++ src/aind_data_schema_models/registries.py
@@ -2,36 +2,14 @@
 
 from typing import Union
 
-from importlib_resources import files
-from pydantic import ConfigDict, Field
+from pydantic import Field
 from typing_extensions import Annotated
 
-from aind_data_schema_models.pid_names import BaseName
-from aind_data_schema_models.utils import create_literal_class, read_csv
+from aind_data_schema_models._generated.registries import _Registry as Registry
+from aind_data_schema_models._generated.registries import _RegistryModel as RegistryModel  # noqa: F401
 
 
-class RegistryModel(BaseName):
-    """Base model config"""
-
-    model_config = ConfigDict(frozen=True)
-
-    name: str = Field(..., title="Registry name")
-    abbreviation: str = Field(..., title="Registry abbreviation")
-
-
-Registry = create_literal_class(
-    objects=read_csv(str(files("aind_data_schema_models.models").joinpath("registries.csv"))),
-    class_name="Registry",
-    base_model=RegistryModel,
-    discriminator="abbreviation",
-    class_module=__name__,
-)
-
-Registry.abbreviation_map = {m().abbreviation: m() for m in Registry.ALL}
-Registry.from_abbreviation = lambda x: Registry.abbreviation_map.get(x)
-
-
-def map_registry(abbreviation: str, record: dict, *args):
+def map_registry(abbreviation: str, record: dict):
     """replace the "registry" key of a dictionary with a RegistryModel object"""
     registry = Registry.from_abbreviation(abbreviation)
     if registry:
